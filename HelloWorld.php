ae392dd547378d402c6b774fed912c356f84ff31
taurus-git <t.1113@ya.ru>
2019-01-12 12:04:17 +0200

    Hello world and variables

---
+++ HelloWorld.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello world</title>
+</head>
+<body>
+
+<?php
+//Hello world and variables
+echo "Hello World!<br />";
+echo "Hello" . " World!<br />";
+
+$greeting = "Hello";
+$target = "World";
+$phrase = $greeting . " " . $target;
+echo $phrase;
+
+?>
+
+</body>
+</html>
